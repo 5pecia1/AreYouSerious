7bef001cba837c353f87e63213ff05e5dd95055c
5pecia1 <5o1.3an9@gmail.com>
2017-11-18 20:18:02 +0900

    make block list

--- background.js
+++ background.js
@@ -16,9 +16,39 @@ var TabsInformation = (function () {
     };
 })();
 
+function checkBlackList(tabsInformation, url, tabId) {
+    var blockList = ['youtube.com'];
+
+    for (var i = 0; i < blockList.length; i++) {
+        if (!!(url.match(new RegExp(blockList[i])))) {
+            var currentHour = new Date().getHours();
+            var currentMinutes = new Date().getMinutes();
+
+            var currentTime = getTime(currentHour, currentMinutes);
+            var startTime = getTime('7', '00');
+            var endTime = getTime(9,0);
+
+            if (currentTime.getTime() <= startTime.getTime()
+                || currentTime.getTime() >= endTime.getTime()) {
+                setOverlay(tabsInformation, url, tabId);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function getTime(hour, minute) {
+    var day = (parseInt(hour) === 0 && parseInt(minute) === 0)? 2 : 1;
+    return new Date(1970, 1, day, hour, minute);
+}
+
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     var tabsInformation= TabsInformation.getInstance();
     if (changeInfo.status === 'complete') {
+        if (checkBlackList(tabsInformation, getUrl(tab.url), tabId)) {
+            return;
+        }
         chrome.storage.sync.get(function (data) {
             var whiteList = data.whiteList;
             var url = getUrl(tab.url);
@@ -42,33 +72,33 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 
                 if (currentTime.getTime() >= startTime.getTime()
                     && currentTime.getTime() <= endTime.getTime()){
-                    if (tabsInformation[tabId] === url) {
-                        delete(tabsInformation[tabId]);
-                        chrome.tabs.executeScript(tabId, {file: "not-overlay.js"});
-                        chrome.tabs.executeScript(tabId, {
-                            code:
-                            'window.areYouSerious = {tabId: ' + tabId + ',' +
-                            'tabUrl: "' + url + '"}'
-                        });
-                    } else {
-                        chrome.tabs.insertCSS(tabId, {file: "overlay.css"});
-                        chrome.tabs.executeScript(tabId, {file: "overlay.js"});
-                        chrome.tabs.executeScript(tabId, {
-                            code:
-                            'window.areYouSerious = {tabId: ' + tabId + ',' +
-                            'tabUrl: "' + url + '"}'
-                        });
-                    }
+                    setOverlay(tabsInformation, url, tabId);
                 }
             }
         });
     }
-
-    function getTime(hour, minute) {
-        var day = (parseInt(hour) === 0 && parseInt(minute) === 0)? 2 : 1;
-        return new Date(1970, 1, day, hour, minute);
-    }
 });
+
+function setOverlay(tabsInformation, url, tabId) {
+    if (tabsInformation[tabId] === url) {
+        delete(tabsInformation[tabId]);
+        chrome.tabs.executeScript(tabId, {file: "not-overlay.js"});
+        chrome.tabs.executeScript(tabId, {
+            code:
+            'window.areYouSerious = {tabId: ' + tabId + ',' +
+            'tabUrl: "' + url + '"}'
+        });
+    } else {
+        chrome.tabs.insertCSS(tabId, {file: "overlay.css"});
+        chrome.tabs.executeScript(tabId, {file: "overlay.js"});
+        chrome.tabs.executeScript(tabId, {
+            code:
+            'window.areYouSerious = {tabId: ' + tabId + ',' +
+            'tabUrl: "' + url + '"}'
+        });
+    }
+}
+
 
 chrome.tabs.onRemoved.addListener( function (tabId, removeInfo) {
     var tabsInformation= TabsInformation.getInstance();
